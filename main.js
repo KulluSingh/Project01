4ded78fc92ba6bc9d23ac76dfd2706f79355469a
KulluSingh <50578465+KulluSingh@users.noreply.github.com>
2020-09-01 21:01:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,5 +8,5 @@ var server = http.createServer(function (request, response) {
 });
 
 // listen on localhost:8000
-server.listen(8000);
-console.log("Server listening on port 8000 :  http://127.0.0.1:8000/");
+server.listen(8050);
+console.log("Server listening on port 8000 :  http://3.135.239.185:8050/");
